aa0a7c5983e241c47c204186c617b9f5dc826788
Yufan Sheng <syhily@gmail.com>
2026-06-05 22:56:45 +0800

    refactor: unify custom icon helpers into lucide-style createIcon

--- src/ui/icons/types.ts
+++ src/ui/icons/types.ts
@@ -6,3 +6,6 @@ export interface IconProps extends Omit<SVGProps<SVGSVGElement>, 'ref'> {
 }
 
 export type LucideIcon = React.ForwardRefExoticComponent<IconProps & RefAttributes<SVGSVGElement>>
+
+export type IconNode = Array<[string, Record<string, string>]>
+
